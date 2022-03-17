bc796073e4af8c0a216ded510dc7fd1521ac9ee6
Leonam Sousa <leonamgm18@gmail.com>
2022-03-17 15:56:42 -0300

    Update List

---
+++ listasEncadeadas/questao02/Celula.java
@@ -0,0 +1,19 @@
+package br.ucsal.listasEncadeadas.questao02;
+
+public class Celula {
+	private Aluno valor;
+	private Celula proximo;
+	
+	public Aluno getValor() {
+		return valor;
+	}
+	public void setValor(Aluno valor) {
+		this.valor = valor;
+	}
+	public Celula getProximo() {
+		return proximo;
+	}
+	public void setProximo(Celula proximo) {
+		this.proximo = proximo;
+	}
+}
